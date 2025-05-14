617f51868e36a5e05184b6c53a7be593d1d765a7
Santiago Emmanuel Tito <santitito14@gmail.com>
2025-05-14 16:12:22 -0300

    first commit

---
+++ Uala Map/Uala_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uala_MapApp.swift
+//  Uala Map
+//
+//  Created by santiago.tito on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Uala_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
